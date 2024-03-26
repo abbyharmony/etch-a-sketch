1ebd37662d706b53b7a89d01f5a2a7dce67ef4d5
Abby Harmony <118316272+abbyharmony@users.noreply.github.com>
2024-03-25 21:57:06 -0400

    Create JS doc and grid through DOM manipulation

---
+++ sketch.js
@@ -0,0 +1,6 @@
+let container = document.getElementsByClassName("container");
+for (let i = 0; i < 257; i++) {
+    let grid = document.createElement("div");
+    container[0].appendChild(grid);
+}
+
