2fe05cf5bc404ec8a4a08af3604ded19f8a68d76
Abby Harmony <118316272+abbyharmony@users.noreply.github.com>
2024-03-27 22:09:44 -0400

    Create event listener for mouseover grid and permahover class

--- sketch.js
+++ sketch.js
@@ -1,6 +1,15 @@
-let container = document.getElementsByClassName("container");
-for (let i = 0; i < 257; i++) {
+let container = document.getElementsByClassName("container")[0];
+for (let i = 0; i < 256; i++) {
     let grid = document.createElement("div");
-    container[0].appendChild(grid);
+    container.appendChild(grid);
 }
 
+container.addEventListener('mouseover', e => {
+    let target = e.target
+  
+    if (target !== container) {
+      target.classList.add('permahover');
+    }
+  });
+
+  
