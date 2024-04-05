4802bbe094dd55eb2e2668431d34dd0a995eafa4
Abby Harmony <118316272+abbyharmony@users.noreply.github.com>
2024-04-05 01:41:13 -0400

    Create event listener for randomize button and add function for color change

--- sketch.js
+++ sketch.js
@@ -1,23 +1,42 @@
 let container = document.getElementsByClassName("container")[0];
+let isRandomized = false;
+
+//create grid squares
 for (let i = 0; i < 256; i++) {
     let grid = document.createElement("div");
+    grid.className = "grid";
     grid.style.width = "45px";
     grid.style.height = "45px";
     container.appendChild(grid);
 }
 
-//create event listener so mouse hover leads to permanent color change
-container.addEventListener('mouseover', e => {
-    let target = e.target
-  
-    if (target !== container) {
-      target.classList.add('permahover');
-    }
-  });
+//Add randomization
+let random = document.getElementById("random");
+let grids = document.getElementsByClassName("grid");
+random.addEventListener("click", () => {
+    isRandomized = true;
+});
 
-//create event listener to personalize grid squares
+//create event listener for color change
+container.addEventListener('mouseover', e => {
+  let target = e.target;
+
+  if (target.classList.contains('grid')) {
+      if (isRandomized) {
+          let colors = ["#ff0000", "#ff7300", "#fffb00", "#48ff00", "#00ffd5", "#002bff", "#7a00ff", "#ff00c8"];
+          let randomColor = colors[Math.floor(Math.random() * colors.length)];
+          target.style.backgroundColor = randomColor;
+      } else {
+          target.style.backgroundColor = "red"; 
+      }
+      target.classList.add('permahover'); 
+  }
+});
+
+//create event listener to choose number of grid squares
 let reset = document.getElementById("reset");
 reset.addEventListener("click", () => {
+  isRandomized = false;
   let inputVal = prompt("Enter number of squares per side for new grid (max 100)");
   if (inputVal < 1 || inputVal > 100 || isNaN(inputVal)) {
     window.alert("Invalid entry");
@@ -25,10 +44,21 @@ reset.addEventListener("click", () => {
    container.innerHTML = "";
     for (let i = 0; i < inputVal * inputVal; i++) {
       let newGrid = document.createElement("div");
+      newGrid.className = "grid";
       newGrid.classList.add("new");
       newGrid.style.width = `${720 / inputVal}px`;
       newGrid.style.height = `${720 / inputVal}px`;
       container.appendChild(newGrid);
+    }
   }
-  }
-})
+});
+
+
+
+ 
+
+
+
+
+    
+   
